39e749376dd118ffbbd53447b437ed4ffa2842a8
Dror Matalon <dror@matal.com>
2023-03-11 11:59:43 -0800

    Made it work when invoked as a symbolic link

--- gish.sh
+++ gish.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+# Description: Gish is a command line tool for interacting with the OpenAI API
+# This script is a wrapper for the node.js script that does the actual work
 source ~/.openai
+# Get the real path to this script in case it is a symlink
+REAL_PATH=`realpath "$0"`
+# Get the directory that this script is in
+BASE_DIR=`dirname "$REAL_PATH"`
+cd $BASE_DIR
 node dist/index.js $*
