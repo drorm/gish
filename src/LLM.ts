abe29dbe4253f2a97a9e6f8f476086e4a6ec2f28
Dror Matalon <dror@matal.com>
2023-03-15 22:14:49 -0700

    Refactor fetch method in LLM class to include streaming response option

--- src/LLM.ts
+++ src/LLM.ts
@@ -1,5 +1,7 @@
 import { Configuration, OpenAIApi } from "openai";
 import * as fs from "fs";
+import chalk from "chalk";
+import { settings } from "./settings.js";
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
@@ -19,12 +21,16 @@ export class LLM {
    * @param query
    * @example const result = await fetch('What is the population of the city of London?');
    */
-  async fetch(query: string) {
-    const fullRequest = query;
+  async fetch(query: string, stream: boolean = true) {
+    if (stream) {
+      return await this.streamResponse(query);
+    }
+
+    // non-streaming response
     try {
       const completion = await this.openai.createChatCompletion({
         model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: fullRequest }],
+        messages: [{ role: "user", content: query }],
       });
       /*
        * data: {
@@ -38,9 +44,20 @@ export class LLM {
        */
 
       const response = completion.data;
-      // console.log(JSON.stringify(response, null, 2));
 
-      return response;
+      let tokens = 0;
+      let text = "";
+      if (response.usage) {
+        tokens = response.usage.total_tokens;
+      }
+      if (
+        response.choices &&
+        response.choices.length > 0 &&
+        response.choices[0].message
+      ) {
+        text = response.choices[0].message["content"];
+      }
+      return { text: text, tokens: tokens };
     } catch (error: any) {
       if (error.response) {
         console.log(error.response.status);
@@ -50,5 +67,82 @@ export class LLM {
       }
       return error;
     }
+  }
+
+  async streamResponse(query: string) {
+    // return a promise with await that resolves to the response
+    return new Promise(async (resolve, reject) => {
+      // based on https://github.com/openai/openai-node/issues/18#issuecomment-1369996933
+      try {
+        let response = "";
+        const res = await this.openai.createChatCompletion(
+          {
+            model: "gpt-3.5-turbo",
+            messages: [{ role: "user", content: query }],
+            stream: true,
+          },
+          { responseType: "stream" }
+        );
+
+        // tell typescript to ignore no 'on'
+        // @ts-ignore
+        res.data.on("data", (data: any) => {
+          const lines = data
+            .toString()
+            .split("\n")
+            .filter((line: string) => line.trim() !== "");
+          for (const line of lines) {
+            const message = line.replace(/^data: /, "");
+            if (message === "[DONE]") {
+              let tokens = 0;
+              process.stdout.write(chalk.green("\n"));
+              if (res.data.usage) {
+                tokens = res.data.usage.total_tokens;
+              }
+              resolve({ text: response, tokens: tokens });
+              return; // Stream finished
+            }
+            try {
+              const parsed = JSON.parse(message);
+              let text = parsed.choices[0].delta.content;
+              if (text) {
+                // convert multiple newlines to a single newline
+                text = text.replace(/\n+/g, "\n");
+                process.stdout.write(chalk.green(text));
+                response += text;
+              }
+            } catch (error) {
+              console.error(
+                "Could not JSON parse stream message",
+                message,
+                error
+              );
+            }
+          }
+        });
+      } catch (error: any) {
+        if (error.response?.status) {
+          console.error(error.response.status, error.message);
+          error.response.data.on("data", (data: any) => {
+            const message = data.toString();
+            try {
+              const parsed = JSON.parse(message);
+              console.error(
+                "An error occurred during OpenAI request: ",
+                parsed
+              );
+            } catch (error) {
+              console.error(
+                "An error occurred during OpenAI request: ",
+                message
+              );
+            }
+          });
+        } else {
+          console.error("An error occurred during OpenAI request", error);
+        }
+        resolve("");
+      }
+    });
   }
 }
