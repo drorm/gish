a2c4dbbfcde6b0689b854b077418fbc18cb11dc4
Dror Matalon <dror@matal.com>
2023-03-19 22:37:11 -0700

    Support for -m --model flag Improve gish.json log file handling

--- src/settings.ts
+++ src/settings.ts
@@ -1,3 +1,7 @@
+import * as os from "os";
+import * as path from "path";
+const homeDir = os.homedir();
+
 export class settings {
   public static CLI_PROMPT =
     "Type help for help.  Control-D to exit, control-c/interrupt to abort.";
@@ -7,7 +11,8 @@ export class settings {
   // export const DIFF_COMMAND = "screen vimdiff"
   // Directory where we put files with chatgpt response
   public static GEN_DIR = "gen";
-  public static LOG_FILE = "gish.json";
+  public static LOG_FILE = path.join(homeDir, `.gish.json`);
   public static TOKEN_COST = 0.002 / 1000; // per https://openai.com/pricing
   public static DEFAULT_EDITOR = "vim";
+  public static DEFAULT_MODEL = "gpt-3.5-turbo";
 }
