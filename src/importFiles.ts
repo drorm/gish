7946ede4eafc88ca33c15f212838cf7152d4a7a0
Dror Matalon <dror@matal.com>
2023-03-23 12:35:41 -0700

    Changed #change to #diff

--- src/importFiles.ts
+++ src/importFiles.ts
@@ -4,9 +4,9 @@ import { Utils } from "./utils.js";
 
 /**
  * Handle #import statements.
- * Give a string, look for "#import" or "#change" statements.
+ * Give a string, look for "#import" or "#diff" statements.
  * #import file: actls like #import or #include file, by replacing the statement with the contents of a file
- * #change does the same but also provides a hint that the result of the request, will need to go into the file
+ * #diff does the same but also provides a hint that the result of the request, will need to go into this file
  */
 export function importFiles(content: string): [boolean, string, string[]] {
   // Split the content into lines
@@ -19,22 +19,22 @@ export function importFiles(content: string): [boolean, string, string[]] {
   let text = ""; // Initialize the text variable
 
   // Initialize an array to store the names of the imported files
-  const toChangeFiles: string[] = [];
+  const toDiffFiles: string[] = [];
 
   // Iterate through the lines of the string
   for (const line of lines) {
     // Match the #import statement using a regular expression
-    const fimport = line.match(/^\s*#import\s+[~\w\\./]+/);
-    // Match the #change statement using a regular expression
-    // Changed files will later by diffed with the output from the bot
-    const fchange = line.match(/^\s*#change\s+[~\w\\./]+/);
-    if (fimport || fchange) {
+    const importFile = line.match(/^\s*#import\s+[~\w\\./]+/);
+    // Match the #diff statement using a regular expression
+    // Diffed files will later by diffed with the output from the bot
+    const diffFile = line.match(/^\s*#diff\s+[~\w\\./]+/);
+    if (importFile || diffFile) {
       // If the line is an #import or #change statement, extract the file name
       const fname = line.split(/\s+/)[1];
       const filePath = Utils.normalizePath(fname); // Normalize the file path
-      if (fchange) {
+      if (diffFile) {
         //If it's a #change statement
-        toChangeFiles.push(filePath); // Append the file name to the files array
+        toDiffFiles.push(filePath); // Append the file name to the files array
       }
 
       try {
@@ -45,7 +45,7 @@ export function importFiles(content: string): [boolean, string, string[]] {
         modifiedLines.push(...fileContents.split("\n"));
       } catch (e) {
         // If the file can't be opened
-        return [false, `#import file ${filePath} was not found`, toChangeFiles];
+        return [false, `#import file ${filePath} was not found`, toDiffFiles];
       }
     } else {
       // If the line is not an #import or # change statement, add it to the modified lines list as is
@@ -54,5 +54,5 @@ export function importFiles(content: string): [boolean, string, string[]] {
   }
 
   // Return: true -- success, Join the modified lines into a single string and the list of changed files
-  return [true, modifiedLines.join("\n"), toChangeFiles];
+  return [true, modifiedLines.join("\n"), toDiffFiles];
 }
