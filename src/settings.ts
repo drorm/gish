6dbcc3d594c3a46549913ddf50b04ac543dc60e2
Dror Matalon <dror@matal.com>
2023-03-31 11:39:26 -0700

    Update CLI_PROMPT message in Settings

--- src/settings.ts
+++ src/settings.ts
@@ -11,7 +11,7 @@ const settingsFile = path.join(gishHome, "settings.json");
  */
 export class Settings {
   public static CLI_PROMPT =
-    "Type help for help.  Control-D to exit, control-c/interrupt to abort.";
+    "Type help for help,  exit to exit, control-c/interrupt to abort.";
   public static DIFF_COMMAND = "vimdiff";
   // Directory where we put files with chatgpt response
   public static GEN_DIR = "gen";
