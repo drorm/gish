03a7df772fbacbe462abfaa400233e427ead2917
Dror Matalon <dror@matal.com>
2023-03-30 20:59:41 -0700

    Add interface message and update LLM class to accept an array of message objects for queries instead of a single string query parameter. Additionally, update fetch and streamResponse functions to use the array of message objects instead of a single query parameter.

--- src/LLM.ts
+++ src/LLM.ts
@@ -4,6 +4,13 @@ import * as os from "os";
 import chalk from "chalk";
 import { countTokens } from "gptoken";
 import { Settings } from "./settings.js";
+
+// interface message  { role: 'assistant', content: 'tell me a joke' } ,
+export interface message {
+  role: string;
+  content: string;
+}
+
 let API_KEY = null;
 if (process.env.OPENAI_API_KEY) {
   API_KEY = process.env.OPENAI_API_KEY;
@@ -28,37 +35,33 @@ if (!API_KEY) {
 const configuration = new Configuration({
   apiKey: API_KEY,
 });
-
 /**
  * The LLM, Long Language Model, class is the main class for the handling the requests to the OpenAI API.
  * It contains the fetch function that calls the OpenAI API.
  */
-
 export class LLM {
   openai = new OpenAIApi(configuration);
   model = Settings.getSetting("DEFAULT_MODEL");
   constructor() {}
-
   /**
    * The fetch function calls the OpenAI API to generate a response to the query.
    * It returns a promise that resolves to the response.
    * @param query
    * @example const result = await fetch('What is the population of the city of London?');
    */
-  async fetch(query: string, options: any, spinner: any) {
+  async fetch(queries: Array<message>, options: any, spinner: any) {
     if (options["model"]) {
       this.model = options["model"];
     }
     const stream = options["stream"];
     if (stream) {
-      return await this.streamResponse(query, options, spinner);
+      return await this.streamResponse(queries, options, spinner);
     }
-
     // non-streaming response
     try {
       const completion = await this.openai.createChatCompletion({
         model: this.model,
-        messages: [{ role: "user", content: query }],
+        messages: <any>queries,
       });
       /*
        * data: {
@@ -70,9 +73,7 @@ export class LLM {
        * usage: { prompt_tokens: 254, completion_tokens: 23, total_tokens: 277 }
        * }
        */
-
       const response = completion.data;
-
       let tokens = 0;
       let text = "";
       if (response.usage) {
@@ -85,7 +86,7 @@ export class LLM {
       ) {
         text = response.choices[0].message["content"];
       }
-      const textWithQuery = query + text;
+      const textWithQuery = queries[0].content + text;
       const estimateTokens = countTokens(textWithQuery);
       return { text: text, tokens: tokens };
     } catch (error: any) {
@@ -98,14 +99,13 @@ export class LLM {
       return error;
     }
   }
-
   /**
    * The streamResponse function calls the OpenAI API and streams the response to the query,
    * so that the response is printed to the console as it is generated.
    * @param query
    * @example const result = await fetch('What is the population of the city of London?');
    */
-  async streamResponse(query: string, options: any, spinner: any) {
+  async streamResponse(queries: Array<message>, options: any, spinner: any) {
     // return a promise with await that resolves to the response
     return new Promise(async (resolve, reject) => {
       // based on https://github.com/openai/openai-node/issues/18#issuecomment-1369996933
@@ -114,7 +114,7 @@ export class LLM {
       let first = true;
       try {
         let response = "";
-        const messages = [{ role: "user", content: query }];
+        const messages = queries;
         let prompt = "";
         if (options["prompt"]) {
           prompt = options["prompt"];
@@ -129,7 +129,6 @@ export class LLM {
           },
           { responseType: "stream" }
         );
-
         // tell typescript to ignore no 'on'
         // @ts-ignore
         res.data.on("data", (data: any) => {
