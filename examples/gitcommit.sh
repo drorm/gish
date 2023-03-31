6174c7237eaab446595da0a36d2ad9e308aa80ef
Dror Matalon <dror@matal.com>
2023-03-31 12:46:24 -0700

    Added new files for editing README, generating TOC, and auto-committing changes.

---
+++ examples/gitcommit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+out="The following is the output of git diff"
+out+=`git diff $*`
+out+="-----------------------------"
+
+out+="The following is the output of git status"
+out+=`git status --untracked-files=no $*`
+out+="-----------------------------"
+
+out+="based on the above provide a commit message"
+git commit -e -m "`echo $out | gish --no-stats`" $*
